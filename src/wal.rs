e8d5926d555723fc88bc5d57584f23788c88ce18
Cameron Lyons <cameron.lyons2@gmail.com>
2026-07-02 08:14:18 -0500

    Update rename column references

--- src/wal.rs
+++ src/wal.rs
@@ -246,14 +246,9 @@ impl WalLog {
                     old_name,
                     new_name,
                 } => {
-                    if let Some(t) = db.tables.get_mut(&table) {
-                        for col in &mut t.columns {
-                            if col.name == new_name {
-                                col.name = old_name;
-                                break;
-                            }
-                        }
-                    }
+                    crate::executor::ddl::rename_column_schema_references(
+                        db, &table, &new_name, &old_name,
+                    );
                 }
                 WalEntry::TruncateTable {
                     name,
@@ -403,14 +398,7 @@ fn rollback_single_entry(entry: WalEntry, db: &mut Database) {
             old_name,
             new_name,
         } => {
-            if let Some(t) = db.tables.get_mut(&table) {
-                for col in &mut t.columns {
-                    if col.name == new_name {
-                        col.name = old_name;
-                        break;
-                    }
-                }
-            }
+            crate::executor::ddl::rename_column_schema_references(db, &table, &new_name, &old_name);
         }
         WalEntry::TruncateTable {
             name,
